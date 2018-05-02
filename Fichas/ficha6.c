986e404e9adeb1d74163d7f9664d6dd719b45922
sir-onze <tiago96baptista@sapo.pt>
2018-05-02 18:23:27 +0100

    Fichas

--- Fichas/ficha6.c
+++ Fichas/ficha6.c
@@ -41,3 +41,38 @@ int top (STACK *s, int *x){
   }
   return 0;
 }
+
+// 2
+// queue sai pela ordem de entrada, inserir no fim retirar no inico, como uma fila de supemercado
+
+void initQueue (QUEUE *q){
+    q->inicio = 0;
+    q->tamnho = 0;
+}
+
+int isEmptyQ (QUEUE *q){
+  if (tamanho==0) return 0;
+  else return 1;
+}
+
+int enqueue (QUEUE *q, int x){
+  int p;
+  if(q->tamanho<MAX){
+  p=q->tamanho+q->inicio;
+  if(p>MAX) p=p-MAX;
+  q->valores[p]=x;
+  q->tamanho++;
+  }
+  else return 1;
+  return 0;
+
+}
+
+int dequeue (QUEUE *q, int *x){
+if(isEmptyQ(q)) return 1;
+*x=q->valores[q->inicio];
+q->inicio++;
+q->tamanho++;
+if(q->inicio==MAX) q->inicio = 0;
+return 0;
+}
--- Fichas/ficha6.h
+++ Fichas/ficha6.h
@@ -4,3 +4,8 @@ typedef struct stack {
 int sp; // aponta para a posicao a inserir na stack
 int valores [MAX]; //conteudo da stack
 } STACK;
+
+typedef struct queue {
+int inicio, tamanho;
+int valores [MAX];
+} QUEUE;
